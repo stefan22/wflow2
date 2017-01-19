4f4e1fcef528145ea59f72a3b17be27992cdb996
Stefan Lissia <stefan.lissia@vml.com>
2017-01-19 14:53:27 +0000

    github api and wrapped ajax call in a DOMContentLoad function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,42 +1,88 @@
-var httpReq = new XMLHttpRequest();
-var user_url = "stefan22";
-
-httpReq.open('GET', 'https://api.github.com/users/' + user_url);
-
-httpReq.onload = function() {
-	//console.log(httpReq.responseText);
-	var resData = JSON.parse(httpReq.responseText);
-	console.log(resData);
-
-
-	var output = document.getElementById('output');
-	output.style.marginTop = "7em";
-	output.style.padding = "1em";
-	output.style.minHeight = "90px";
-	output.style.backgroundColor = "#ffffff";
-	output.style.borderRadius = "4px";
-
-	
-
-	//grab image
-	var usr_avatar = resData.avatar_url;
-	
-	output.innerHTML = "<img style='max-width:200px;max-height:200px;margin:0 auto 15px;' src=" + usr_avatar +  "/>";
-	output.innerHTML += "<p>Account type: " + resData.type + "</p>";
-	output.innerHTML += "<p>User name: " + resData.name + "</p>";
-	output.innerHTML += "<p>Public repos: " + resData.public_repos + "</p>";
-	output.innerHTML += "<p>Location: " + resData.location + "</p>";
-	output.innerHTML += "<p>Followers " + resData.followers + "</p>";
-	output.innerHTML += "<p>Following " + resData.following + "</p>";
 
 
 
+function dataload() {
+		console.log('function called');
+		var httpReq = new XMLHttpRequest();
+		var user_url = "stefan22";
+		var repo_type = "javascript";
+
+		httpReq.open('GET', 'https://api.github.com/users/' + user_url);
+
+		httpReq.onload = function() {
+				//console.log(httpReq.responseText);
+				var resData = JSON.parse(httpReq.responseText);
+				console.log(resData);
+
+
+				var output = document.getElementById('output');
+				output.style.marginTop = ".5em";
+				output.style.minHeight = "90px";
+				output.style.outline = "1px groove #465780";
+
+				//grab image
+				var usr_avatar = resData.avatar_url;
+				//console.log(usr_avatar);
+				
+				output.innerHTML = "<img src=" + usr_avatar + " style='margin:0 auto 15px; border-radius:4px;box-shadow:0px 1px 3px 1px rgba(0,0,0,.5);opacity:.9;'" 
+				+ "/>";
+				
+
+
+				//add number of repos to field 1
+				var phone = document.getElementById('phone').value = resData.public_repos;
+				
+				//add name to name field:
+				var gitname = document.getElementById('gitname').value = resData.name;
+				// add account type
+				var accType = document.getElementById('accType').value = resData.type;
+				// add account location
+				var accloc = document.getElementById('accloc').value = resData.location;
+				// add user followers
+				var followers = document.getElementById('followers').value = resData.followers;
+			   	// add user following
+				var following = document.getElementById('following').value = resData.following;
+				// add user blog
+				var blog = document.getElementById('blog').value = resData.blog;
+				// add user url
+				var url = document.getElementById('url').value = resData.html_url;
 
 
 
-};
+				//get time account was created
+				var startDat = (resData.created_at);
+				console.log(startDat);
+				//convert string to date object and to miliseconds
+				var startObj = new Date(startDat).getTime();
+				console.log(startObj);
+				var today = new Date().getTime();
+				console.log(today);
+				var difDat = Math.abs(today - startObj);
+				console.log(difDat);  //94698848067
+
+
+
+
+		};
 
 httpReq.send();
+
+
+
+
+}//dataload
+
+document.addEventListener('DOMContentLoaded', dataload, false);
+
+
+
+	
+	
+
+
+
+
+
 
 
 
